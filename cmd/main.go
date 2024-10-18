050d9f4aaaf411d438f6298c979cb435baffa1c2
FedericoIglesias <igl.fede@gmail.com>
2024-10-18 14:23:17 +0100

    featCreateAlias

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,27 @@
 package main
 
-import (
-	"goWallet/cmd/api/routes"
-	"log"
-	"os"
+import "goWallet/internal/services"
 
-	"github.com/joho/godotenv"
-)
+// "goWallet/cmd/api/routes"
+// "log"
+// "os"
+
+// "github.com/joho/godotenv"
 
 func main() {
 
-	if err := godotenv.Load("./.env"); err != nil {
-		log.Fatalf("Error: %s", err)
-	}
+	str := services.CreateAlias()
 
-	r := routes.SetupRouter()
+	println(str)
 
-	PORT := os.Getenv("PORT")
+	// if err := godotenv.Load("./.env"); err != nil {
+	// 	log.Fatalf("Error: %s", err)
+	// }
 
-	r.Run(":" + PORT)
+	// r := routes.SetupRouter()
+
+	// PORT := os.Getenv("PORT")
+
+	// r.Run(":" + PORT)
 
 }
--- internal/services/register.go
+++ internal/services/register.go
@@ -3,12 +3,15 @@ package services
 import (
 	"fmt"
 	"goWallet/internal/domain"
+	"os"
+	"strings"
 
 	"math/rand"
 )
 
 func (s Services) Register(user *domain.User) error {
 	user.Cvu = CreateCVU()
+	user.Alias = CreateAlias()
 	return s.Repo.Register(user)
 }
 
@@ -16,4 +19,18 @@ func CreateCVU() string {
 	formatted := fmt.Sprintf("%019d", rand.Uint64())
 
 	return ("996" + formatted)
+}
+
+func CreateAlias() string {
+	file, err := os.ReadFile("./internal/services/alia.csv")
+	if err != nil {
+		return err.Error()
+	}
+
+	listWord := strings.Split(string(file), "\n")
+
+	alias := listWord[rand.Intn(100)]
+
+	return alias
+
 }
